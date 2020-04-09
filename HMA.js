59e128aa20f4dcd57cef4e2cd6595282dd773ef3
Hamza Mustafa Alvi <37804952+hamzaMAlvi@users.noreply.github.com>
2020-04-09 21:16:45 +0500

    Component along with image required by component was added

---
+++ HMA.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HMA extends Component {
+  render() {
+    return (
+      <div>
+        <img src={ require('./FAST_newbuilding.jpg') } height='400' width='600' />
+        <h2>Hamza Mustafa Alvi</h2>
+      </div>
+    );
+  }
+}
+export default HMA;
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HMA from './HMA.js'
+
+ReactDOM.render(<HMA />, document.getElementById('root'));
